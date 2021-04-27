1206991b084041395d071fe841105e426a70a028
Marek Matczak <mmatczak@autograf.pl>
2021-04-27 18:36:47 +0200

    Persistence adapter migrated to DynamoDB

--- src/adapter/out/persistence/index.ts
+++ src/adapter/out/persistence/index.ts
@@ -4,55 +4,62 @@ import { ObjectNotFoundError } from '../../../application/common';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import AWS from 'aws-sdk';
 
-let books: BookState[] = [
-  { id: '5VVGTSPMKHH3A8', author: 'Douglas Crockford', title: 'JavaScript: The Good Parts', isbn: '9780596517748' },
-  { id: '5VVGTSPMKHH3A9', author: 'Joshua Bloch', title: 'Effective Java', isbn: '9780134685991' },
-  { id: '5VVGTSPMKHH3A0', author: 'Robert C. Martin', title: 'Clean Code', isbn: '9780132350884' },
-  { id: '5VVGTSPMKHH3A1', author: 'Eric Evans', title: 'Domain-Driven Design', isbn: '9780321125217' },
-];
-
 export interface DynamoDbConfig {
   endpoint?: string;
   region: string;
   table: string;
 }
 
-export function loadBookFactory(): loadBook {
+export function loadBookFactory(config: DynamoDbConfig): loadBook {
+  const dynamodb = createDynamoDbClient(config);
+
   return function (id: BookId): Promise<BookState> {
-    const book = books.find((book) => book.id === id.value);
-    return book ? Promise.resolve(book) : Promise.reject(new ObjectNotFoundError<string>('Book', id.value));
-  };
-}
-
-export function loadAllBooksFactory({ endpoint, region, table }: DynamoDbConfig): loadAllBooks {
-  const dynamodb: DocumentClient = new AWS.DynamoDB.DocumentClient({
-    region,
-    endpoint,
-  });
-
-  return function (): Promise<BookState[]> {
     return dynamodb
-      .scan({
-        TableName: table,
+      .get({
+        TableName: config.table,
+        Key: { id: id.value },
       })
       .promise()
-      .then((books) => {
-        console.log(books.Items);
-        return books.Items as BookState[];
+      .then(({ Item: book }) => {
+        if (!book) {
+          throw new ObjectNotFoundError<string>('Book', id.value);
+        }
+        return book as BookState;
       });
   };
 }
 
-export function persistUpdatedBookFactory(): persistUpdatedBook {
-  return function (bookToUpdate: BookState): Promise<void> {
-    books = books.map((book) => (book.id === bookToUpdate.id ? bookToUpdate : book));
-    return Promise.resolve();
+export function loadAllBooksFactory(config: DynamoDbConfig): loadAllBooks {
+  const dynamodb = createDynamoDbClient(config);
+
+  return function (): Promise<BookState[]> {
+    return dynamodb
+      .scan({
+        TableName: config.table,
+      })
+      .promise()
+      .then((books) => books.Items as BookState[]);
   };
 }
 
-export function persistNewBookFactory(): persistNewBook {
-  return function (bookToAdd: BookState): Promise<void> {
-    books = [...books, bookToAdd];
-    return Promise.resolve();
+export const persistUpdatedBookFactory = saveOrUpdateFactory;
+
+export const persistNewBookFactory = saveOrUpdateFactory;
+
+function saveOrUpdateFactory(config: DynamoDbConfig): persistUpdatedBook | persistNewBook {
+  const dynamodb = createDynamoDbClient(config);
+
+  return function (book: BookState): Promise<void> {
+    return dynamodb
+      .put({
+        TableName: config.table,
+        Item: book,
+      })
+      .promise()
+      .then(() => undefined);
   };
+}
+
+function createDynamoDbClient({ region, endpoint }: DynamoDbConfig): DocumentClient {
+  return new AWS.DynamoDB.DocumentClient({ region, endpoint });
 }
